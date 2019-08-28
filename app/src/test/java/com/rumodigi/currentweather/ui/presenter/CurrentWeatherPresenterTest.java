e7253c04a68eb3f3c9f0d7dfab83351b038f6f0e
Russell Morris <russell.morris@cybg.com>
2019-08-28 22:19:12 +0100

    Added more presenter tests

--- app/src/test/java/com/rumodigi/currentweather/ui/presenter/CurrentWeatherPresenterTest.java
+++ app/src/test/java/com/rumodigi/currentweather/ui/presenter/CurrentWeatherPresenterTest.java
@@ -1,23 +1,39 @@
 package com.rumodigi.currentweather.ui.presenter;
 
+import android.app.Activity;
+import android.location.Location;
+
 import com.rumodigi.currentweather.framework.location.LocationHandler;
 import com.rumodigi.currentweather.framework.location.LocationResultListener;
 import com.rumodigi.currentweather.ui.view.CurrentWeatherView;
+import com.rumodigi.domain.models.CurrentlyModel;
+import com.rumodigi.domain.models.ForecastModel;
 import com.rumodigi.domain.usecases.GetForecastDetailsUseCase;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import io.reactivex.observers.DisposableSingleObserver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CurrentWeatherPresenterTest {
 
     private CurrentWeatherPresenter<CurrentWeatherView> currentWeatherPresenter;
+    private double latitude = 55.8685531;
+    private double longitude = -4.2635288;
 
     @Mock
     LocationHandler mockLocationHandler;
@@ -27,9 +43,18 @@ public class CurrentWeatherPresenterTest {
     CurrentWeatherView mockCurrentWeatherView;
     @Mock
     LocationResultListener mockLocationResultListener;
+    @Mock
+    Location mockLocation;
+    @Mock
+    ForecastModel mockForecastModel;
+    @Mock
+    CurrentlyModel mockCurrentlyModel;
+
 
     @Before
     public void setUp() {
+        when(mockLocation.getLatitude()).thenReturn(latitude);
+        when(mockLocation.getLongitude()).thenReturn(longitude);
         currentWeatherPresenter = new CurrentWeatherPresenter<>(mockLocationHandler, mockGetForecastDetailsUseCase);
         currentWeatherPresenter.onViewCreated(mockCurrentWeatherView);
         currentWeatherPresenter.setLocationResultListener(mockLocationResultListener);
@@ -45,5 +70,114 @@ public class CurrentWeatherPresenterTest {
         verify(mockCurrentWeatherView).hideErrorMessage();
         verify(mockCurrentWeatherView).showProgressSpinner();
         verify(mockLocationHandler).getUserLocation();
+    }
+
+    @Test
+    public void whenCorrectPermissionReceived_locationIsRequested(){
+        currentWeatherPresenter.permissionResultsReceived(LocationHandler.LOCATION_REQUEST_CODE);
+        verify(mockCurrentWeatherView).hideErrorMessage();
+        verify(mockCurrentWeatherView).showProgressSpinner();
+        verify(mockLocationHandler).getUserLocation();
+    }
+
+    @Test
+    public void whenIncorrectPermissionReceived_locationIsNotRequested(){
+        currentWeatherPresenter.permissionResultsReceived(9999);
+        verifyZeroInteractions(mockCurrentWeatherView);
+    }
+
+    @Test
+    public void whenLocationServicesIsGranted_locationIsRequested(){
+        currentWeatherPresenter.resultReceived(LocationHandler.LOCATION_REQUEST_CODE, Activity.RESULT_OK);
+        verify(mockCurrentWeatherView).hideErrorMessage();
+        verify(mockCurrentWeatherView).showProgressSpinner();
+        verify(mockLocationHandler).getUserLocation();
+    }
+
+    @Test
+    public void whenLocationServicesIsDenied_locationIsRequested(){
+        currentWeatherPresenter.resultReceived(LocationHandler.LOCATION_REQUEST_CODE, Activity.RESULT_CANCELED);
+        verify(mockCurrentWeatherView).showErrorMessage();
+        verify(mockCurrentWeatherView).hideProgressSpinner();
+    }
+
+    @Test
+    public void whenPermissionHasBeenDeniedBefore_showRetryMessageAndRetryButton(){
+        currentWeatherPresenter.locationPermissionPreviouslyDenied();
+        verify(mockCurrentWeatherView).hideProgressSpinner();
+        verify(mockCurrentWeatherView).hideErrorMessage();
+        verify(mockCurrentWeatherView).hideUpdateForecastButton();
+        verify(mockCurrentWeatherView).showRetryMessage();
+        verify(mockCurrentWeatherView).showPermissionRetryButton();
+    }
+
+    @Test
+    public void whenPermissionHasBeenDeniedBeforeWithNeverAskAgain_showGotoSettingsMessage(){
+        currentWeatherPresenter.locationPermissionPreviouslyDeniedWithNeverAskAgain();
+        verify(mockCurrentWeatherView).hideProgressSpinner();
+        verify(mockCurrentWeatherView).hideErrorMessage();
+        verify(mockCurrentWeatherView).hideUpdateForecastButton();
+        verify(mockCurrentWeatherView).hidePermissionRetryButton();
+        verify(mockCurrentWeatherView).hideUpdateForecastButton();
+        verify(mockCurrentWeatherView).showGotoSettingsMessage();
+    }
+
+    @Test
+    public void whenLocationIsFound_refreshWeatherData_summaryIsClear_noPreciptiation(){
+        when(mockCurrentlyModel.getSummary()).thenReturn("Clear");
+        when(mockCurrentlyModel.getPrecipType()).thenReturn(null);
+        when(mockForecastModel.getCurrentlyModel()).thenReturn(mockCurrentlyModel);
+        final ArgumentCaptor<DisposableSingleObserver> observerArgumentCaptor = ArgumentCaptor.forClass(DisposableSingleObserver.class);
+        currentWeatherPresenter.refreshWeatherDetails(mockLocation);
+        verify(mockGetForecastDetailsUseCase).execute(observerArgumentCaptor.capture(), anyDouble(), anyDouble());
+        DisposableSingleObserver capturedObserver = observerArgumentCaptor.getValue();
+        capturedObserver.onSuccess(mockForecastModel);
+        verify(mockCurrentWeatherView).hideErrorMessage();
+        verify(mockCurrentWeatherView).hideRetryMessage();
+        verify(mockCurrentWeatherView).hideGotoSettingsMessage();
+        verify(mockCurrentWeatherView).hidePermissionRetryButton();
+        verify(mockCurrentWeatherView).showUpdateForecastButton();
+        verify(mockCurrentWeatherView).updateDateAndTime(anyString());
+        verify(mockCurrentWeatherView).updateLatLong(anyDouble(), anyDouble());
+        verify(mockCurrentWeatherView).updateSummary(anyString());
+        verify(mockCurrentWeatherView).updateTemp(anyDouble());
+        verify(mockCurrentWeatherView).noPrecipitation();
+        verify(mockCurrentWeatherView).updateCloudCover(anyString());
+        verify(mockCurrentWeatherView).hideProgressSpinner();
+    }
+
+    @Test
+    public void whenLocationIsFound_refreshWeatherData_summaryIsCloudy_itIsRaining(){
+        when(mockCurrentlyModel.getSummary()).thenReturn("Cloudy");
+        when(mockCurrentlyModel.getPrecipType()).thenReturn("Rain");
+        when(mockForecastModel.getCurrentlyModel()).thenReturn(mockCurrentlyModel);
+        final ArgumentCaptor<DisposableSingleObserver> observerArgumentCaptor = ArgumentCaptor.forClass(DisposableSingleObserver.class);
+        currentWeatherPresenter.refreshWeatherDetails(mockLocation);
+        verify(mockGetForecastDetailsUseCase).execute(observerArgumentCaptor.capture(), anyDouble(), anyDouble());
+        DisposableSingleObserver capturedObserver = observerArgumentCaptor.getValue();
+        capturedObserver.onSuccess(mockForecastModel);
+        verify(mockCurrentWeatherView).hideErrorMessage();
+        verify(mockCurrentWeatherView).hideRetryMessage();
+        verify(mockCurrentWeatherView).hideGotoSettingsMessage();
+        verify(mockCurrentWeatherView).hidePermissionRetryButton();
+        verify(mockCurrentWeatherView).showUpdateForecastButton();
+        verify(mockCurrentWeatherView).updateDateAndTime(anyString());
+        verify(mockCurrentWeatherView).updateLatLong(anyDouble(), anyDouble());
+        verify(mockCurrentWeatherView).updateSummary(anyString());
+        verify(mockCurrentWeatherView).updateTemp(anyDouble());
+        verify(mockCurrentWeatherView).updatePrecipitation(anyString(), anyString());
+        verify(mockCurrentWeatherView).updateCloudCover(anyString());
+        verify(mockCurrentWeatherView).hideProgressSpinner();
+    }
+
+    @Test
+    public void whenLocationIsNotFound_showErrorMessage(){
+        final ArgumentCaptor<DisposableSingleObserver> observerArgumentCaptor = ArgumentCaptor.forClass(DisposableSingleObserver.class);
+        currentWeatherPresenter.refreshWeatherDetails(mockLocation);
+        verify(mockGetForecastDetailsUseCase).execute(observerArgumentCaptor.capture(), anyDouble(), anyDouble());
+        DisposableSingleObserver capturedObserver = observerArgumentCaptor.getValue();
+        capturedObserver.onError(new Throwable());
+        verify(mockCurrentWeatherView).hideProgressSpinner();
+        verify(mockCurrentWeatherView).showErrorMessage();
     }
 }
